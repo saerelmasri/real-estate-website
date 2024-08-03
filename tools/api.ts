866f707b5da488c3f5c51562d7e4d384214c51d0
saerelmasri <saer1890@gmail.com>
2024-08-03 18:41:18 +0300

    fixes

--- tools/api.ts
+++ tools/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.API_URL,
     headers: {
         'ContentType': 'application/json',
     },
